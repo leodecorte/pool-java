de1fda52b8ccdc2f7d1a802b15624691295dd0e7
leodecorte <leodecorte@hotmail.com>
2024-11-04 23:28:42 +0100

    Create a function named “myConcat” that takes two parameters.
    The function must display the first parameter followed by a space followed by the second parameter.

---
+++ pool_java_d01/ex_01/ex_01.java
@@ -0,0 +1,10 @@
+
+public class ex_01 {
+    public static void main(String[] args) {
+        myConcat("Hello", "world");
+    }
+
+    public static void myConcat(String str1, String str2) {
+        System.out.print(str1 + " " + str2);
+    }
+}
